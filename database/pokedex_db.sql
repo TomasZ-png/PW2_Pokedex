b24a2b55625c1b36c6155d0c101a2ec2d2969c44
TomasZ-png <animacionesc4d@gmail.com>
2025-09-09 15:36:57 -0300

    primer commit

---
+++ database/pokedex_db.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS pokedex_db;
+
+use pokedex_db;
+
+CREATE TABLE pokemones (
+    id_pokemon INT AUTO_INCREMENT PRIMARY KEY,
+    numero INT NOT NULL,
+    nombre VARCHAR(100) NOT NULL,
+    tipo VARCHAR(100) NOT NULL,
+    descripcion TEXT,
+    imagen VARCHAR(255)
+);
