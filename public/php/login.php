ff80836265893fa42f51f353184728f3c255d254
Facundo Caruso <facundohernancaruso@gmail.com>
2025-09-18 17:59:06 -0300

    Agregadas img  de pokemones, insert de c/u, favicon en vistas y cambio en botones y style home

--- public/php/login.php
+++ public/php/login.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="\PW2_Pokedex\src\img\favicon.ico">
     <title>Iniciar Sesion</title>
 </head>
 <body>
